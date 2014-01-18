08019214c0a734f27afc2eaf1ecee56042af9ef3
Jeff Philip Goodhue <goodhuj2@quest-OptiPlex-9010>
2014-01-17 17:17:40 -0800

    Committer: Jeff Philip Goodhue <goodhuj2@quest-OptiPlex-9010>
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   makefile
    	modified:   randomspheres.c
    	modified:   raytrace.c
    	modified:   raytrace.h
    	modified:   ring
    	modified:   ring.c
    	modified:   test
    	modified:   testraytrace.c
    	modified:   testvectors.c
    	modified:   vectors.c
    	modified:   vectors.h
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	.raytrace.h.swp
    	.ring.c.swp

--- vectors.c
+++ vectors.c
@@ -11,46 +11,48 @@ Geoffrey Matthews
 */
 
 vector vectorMalloc(void) {
-  vector v = (vector)malloc(sizeof(struct vector));
-  if (v == NULL) {
-    printf("ERROR in vectorMalloc.\n");
-    exit(-1);
-  }
-  v->x = 0.0; v->y = 0.0; v->z = 0.0;
-  return v;
+//  vector v = (vector)malloc(sizeof(struct vector));
+//  if (v == NULL) {
+//    printf("ERROR in vectorMalloc.\n");
+//    exit(-1);
+//  }
+//  v->x = 0.0; v->y = 0.0; v->z = 0.0;
+//  return v;
+  return (struct vector){0, 0, 0};
 }
 
 void vectorFree(vector v) {
-  free(v);
+//  free(v);
+//  Do nothing here...
 }
 
-void assign(vector target, /* out */
+void assign(vector* target, /* out */
 	    double x, double y, double z /* in */) {
   *target = (struct vector){x,y,z};
 }
 
-void copy(vector target, /* out */
+void copy(vector* target, /* out */
 	  vector source  /* in */) {
-  target->x = source->x; target->y = source->y; target->z = source->z;
+  target->x = source.x; target->y = source.y; target->z = source.z;
 }
 
-void add(vector target, /* out */
+void add(vector* target, /* out */
 	 vector source1, /* in */
 	 vector source2  /* in */) {
-  target->x = source1->x + source2->x;
-  target->y = source1->y + source2->y;
-  target->z = source1->z + source2->z;
+  target->x = source1.x + source2.x;
+  target->y = source1.y + source2.y;
+  target->z = source1.z + source2.z;
 }
 
-void subtract(vector target, /* out */
+void subtract(vector* target, /* out */
 	      vector source1, /* in */
 	      vector source2  /* in */) {
-  target->x = source1->x - source2->x;
-  target->y = source1->y - source2->y;
-  target->z = source1->z - source2->z;
+  target->x = source1.x - source2.x;
+  target->y = source1.y - source2.y;
+  target->z = source1.z - source2.z;
 }
 
-void multiply(vector target, /* inout */
+void multiply(vector* target, /* inout */
 	      double s       /* in */) {
   target->x *= s;
   target->y *= s;
@@ -59,7 +61,7 @@ void multiply(vector target, /* inout */
 
 double dot(vector v1, vector v2) {
 /* return the dot product of v1 and v2 */
-  return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+  return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
 double mag(vector v) {
@@ -67,10 +69,10 @@ double mag(vector v) {
   return sqrt(dot(v, v));
 }
 
-void normalize(vector v) {
+void normalize(vector* v) {
 /* set v to unit length
    does not change zero length vectors */
-  double m = mag(v);
+  double m = mag(*v);
   if (m > 0.0) {
     multiply(v, 1.0/m);
   }
@@ -78,19 +80,19 @@ void normalize(vector v) {
 
 double distance(vector p1, vector p2) {
 /* return the distance between points p1 and p2 */
-  vector v = vectorMalloc();
-  subtract(v, p1, p2);
+  vector v;
+  subtract(&v, p1, p2);
   return mag(v);
 }
 
 void vectorPrint(vector v) {
 /* readable print for v */
-  printf("vector(%0.2f, %0.2f, %0.2f)", v->x, v->y, v->z);
+  printf("vector(%0.2f, %0.2f, %0.2f)", v.x, v.y, v.z);
 }
 
 void vectorIntegerPrintf(FILE *file, vector color) {
 /* output just the rounded values of the vector to the file */
-  fprintf(file, "%d %d %d\n", (int)color->x, (int)color->y, (int)color->z);
+  fprintf(file, "%d %d %d\n", (int)color.x, (int)color.y, (int)color.z);
 }
 
 
--- vectors.h
+++ vectors.h
@@ -12,26 +12,26 @@ typedef struct vector{
   double x;
   double y;
   double z;
-} *vector;
+} vector;
 
 vector vectorMalloc(void);
 void vectorFree(vector v);
-void assign(vector target, /* inout */
+void assign(vector* target, /* inout */
 	    double x, double y, double z /* in */);
-void copy(vector target, vector source);
-void add(vector target,  /* out */
+void copy(vector* target, vector source);
+void add(vector* target, /* out */
 	 vector source1, /* in */
 	 vector source2  /* in */);
-void subtract(vector target,  /* out */
+void subtract(vector* target,  /* out */
 	      vector source1, /* in */
 	      vector source2  /* in */);
-void multiply(vector target, /* inout */
+void multiply(vector* target, /* inout */
 	      double s       /* in */);
 double dot(vector v1, vector v2);
 /* return the dot product of v1 and v2 */
 double mag(vector v);
 /* return the magnitude of v */
-void normalize(vector v /* inout */);
+void normalize(vector* v /* inout */);
 /* set v to unit length
    does not change zero length vectors */
 double distance(vector p1, vector p2);
